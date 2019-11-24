b7450ce201219d7758e6dd0bcc7662333cc0ab6d
Upota <yujihoon3510@gmail.com>
2019-11-24 21:10:13 +0900

    B++ complete

--- b.ml
+++ b.ml
@@ -270,15 +270,16 @@ and eval : env -> memory -> exp -> (value * memory)
   | CALLV (x, lst) ->
     let (idlst, ee, en) = lookup_proc_env x env in
     let (r, m, ex_mem) = 
-    list_fold2 (fun (re, me, ex_m) x e ->
-      let (v, mem1) = eval env me e in
+    list_fold2 (fun (re, me, ex_m) x es ->
+      let (v, mem1) = eval env me es in
       let l = new_location () in
       let reco = extend_env (LocBind (x,l)) re in
       let ex_me = extend_mem (l,v) ex_m in
       (reco, mem1, ex_me)) (empty_env, mem, empty_mem) idlst lst in
-    let env_eval = en @ r @[ProcBind (x, (idlst, ee, en))] in
+    let env_eval = [ProcBind (x, (idlst, ee, en))] @ r @ en in
     let mem_eval = m @ ex_mem in
-    eval env_eval mem_eval ee
+    let (v_, mem_) = eval env_eval mem_eval ee in
+    (v_, (mem_ @ mem))
   | CALLR (f, lst) ->
     let pro = lookup_proc_env f env in
       begin match pro with
@@ -315,7 +316,7 @@ and eval : env -> memory -> exp -> (value * memory)
     let mem2 = extend_mem (l, v1) mem1 in
     (v1, mem2)
   | ASSIGNF (e1, x, e2) ->
-    begin match eval env mem e with
+    begin match eval env mem e1 with
     | (Record r, mem1) ->
       let (v, mem2) = eval env mem1 e2 in
       let l = lookup_record x r in
@@ -326,6 +327,8 @@ and eval : env -> memory -> exp -> (value * memory)
 
 and gc : env -> memory -> memory
 = fun env mem ->
+  (* let _ = print_endline (env2str env) in
+  let _ = print_endline (mem2str mem) in  *)
   let reach = list_fold  (fun reach_ hd ->
     begin match hd with
     | LocBind (x,l) -> extend_reach l reach_ mem
@@ -362,39 +365,22 @@ let runb : exp -> value
   let _ = print_endline ("memory size: " ^ string_of_int (size_of_mem m)) in
     v;;
 
-runb (LETV ("ret", NUM 1,
-LETV ("n", NUM 5,
-SEQ (
-WHILE (LESS (NUM 0, VAR "n"),
-SEQ (
-ASSIGN ("ret", MUL (VAR "ret", VAR "n")),
-ASSIGN ("n", SUB (VAR "n", NUM 1))
-)
-),
-VAR "ret")))
-);;
-
-runb (LETF ("f", ["x1"; "x2"],
-SEQ (
-ASSIGN ("x1", NUM 3),
-ASSIGN ("x2", NUM 3)
-),
-LETV("x1", NUM 1,
-LETV("x2", NUM 1,
-SEQ(
-CALLR ("f", ["x1"; "x2"]),
-ADD(VAR "x1", VAR "x2"))))));;
-
-runb (LETV ("f", RECORD ([("x", NUM 10); ("y", NUM 13)]),
-LETF ("swap", ["a"; "b"],
-LETV ("temp", VAR "a",
-SEQ (
-ASSIGN ("a", VAR "b"),
-ASSIGN ("b", VAR "temp"))),
-SEQ (
-CALLV("swap", [FIELD (VAR "f", "x"); FIELD (VAR "f", "y")]),
-FIELD (VAR "f", "x")
-)
-)
-)
-);;
+runb(IF(EQUAL(ADD(NUM 1,NUM 3),DIV(NUM 12,SUB(NUM 4,NUM 1))),TRUE,FALSE));;
+(* gc debug *)
+runb(LETF("f",["x1";"x2"],SEQ(ASSIGN("x1",NUM 3),ASSIGN("x2",NUM 3)),LETV("x1",NUM 1,LETV("x2",NUM 1,SEQ(CALLV("f",[VAR "x1";VAR "x2"]),ADD(VAR "x1",VAR "x2"))))));;
+runb(LETV("ret",NUM 1,LETV("n",NUM 5,SEQ(WHILE(LESS(NUM 0,VAR "n"),SEQ(ASSIGN("ret",MUL(VAR "ret",VAR "n")),ASSIGN("n",SUB(VAR "n",NUM 1)))),VAR "ret"))));;
+runb(LETV("x",NUM 2,SEQ(ASSIGN("x",NUM 1),VAR "x")));;
+runb(LETF("f",["x1";"x2"],SEQ(ASSIGN("x1",NUM 3),ASSIGN("x2",NUM 3)),LETV("x1",NUM 1,LETV("x2",NUM 1,SEQ(CALLR("f",["x1";"x2"]),ADD(VAR "x1",VAR "x2"))))));;
+runb(LETV("x",NUM 1,LETV("y",NUM 2,LETF("f",["x1";"x2"],SEQ(SEQ(ASSIGN("x1",MUL(VAR "x1",NUM 2)),ASSIGN("x2",ADD(VAR "x2",NUM 2))),ADD(VAR "x1",VAR "x2")),SEQ(CALLR("f",["x";"y"]),LETV("r",ADD(VAR "x",VAR "y"),ADD(VAR "r",CALLV("f",[VAR "x";VAR "y"]))))))));;
+runb(LETV("n",NUM 5,LETV("sum",NUM 0,LETF("calc",[],WHILE(NOT(EQUAL(VAR "n",NUM 0)),SEQ(ASSIGN("sum",ADD(VAR "sum",VAR "n")),ASSIGN("n",SUB(VAR "n",NUM 1)))),SEQ(CALLR("calc",[]),VAR "sum")))));;
+runb(LETF("f",["n"],IF(LESS(VAR "n",NUM 2),NUM 1,ADD(CALLV("f",[SUB(VAR "n",NUM 1)]),CALLV("f",[SUB(VAR "n",NUM 2)]))),CALLV("f",[NUM 5])));;
+runb(LETV("n",NUM 10,LETF("f",["x"],IF(EQUAL(VAR "n",NUM 0),UNIT,SEQ(SEQ(ASSIGN("x",ADD(VAR "x",NUM 1)),ASSIGN("n",SUB(VAR "n",NUM 1))),CALLR("f",["x"]))),LETV("x",NUM 10,SEQ(CALLR("f",["x"]),VAR "x")))));;
+runb(LETV("x",NUM 5,LETV("y",NUM 5,LETF("f",["n"],SEQ(ASSIGN("n",ADD(VAR "n",NUM 1)),UNIT),ADD(SEQ(CALLR("f",["x"]),VAR "x"),SEQ(CALLV("f",[VAR "y"]),VAR "y"))))));;
+runb(LETF("swap",["a";"b"], LETV("temp",VAR "a",SEQ(ASSIGN("a",VAR "b"),ASSIGN("b",VAR "temp"))),LETV("x",NUM 1,LETV("y",NUM 2,SEQ(CALLR("swap",["x";"y"]),VAR "x")))));;
+runb(FIELD(RECORD([("id",NUM 123);("pw",NUM 123)]),"id"));;
+runb(LETV("f",RECORD([("x",TRUE);("y",FALSE)]),SEQ(ASSIGNF(VAR "f","y",NOT (NOT TRUE)),FIELD(VAR "f","y"))));;
+runb(LETV("f",RECORD([("x",NUM 5);("y",NUM 7)]),LETV("x",VAR "f",SEQ(ASSIGNF(VAR "f","x",NUM 6),FIELD(VAR "x","x")))));;
+runb(LETV("x",NUM 1,LETV("f",RECORD([("x",NUM 1);("y",VAR "x")]),FIELD(VAR "f","y"))));;
+runb(LETV("x",RECORD([("x",NUM 5);("y",NUM 7)]),SEQ(ASSIGNF(VAR "x","x",ADD(FIELD(VAR "x","x"),FIELD(VAR "x","y"))),FIELD(VAR "x","x"))));;
+runb(LETV("f",RECORD([("x",NUM 10);("y",NUM 13)]),LETF("swap",["a";"b"],LETV("temp",VAR "a",SEQ(ASSIGN("a",VAR "b"),ASSIGN("b",VAR "temp"))),SEQ(CALLV("swap",[FIELD(VAR "f","x");FIELD(VAR "f","y")]),FIELD(VAR "f","x")))));;
+runb(LETV("num",RECORD([("n",NUM 5);("is_even",FALSE)]),LETF("f",["n"],WHILE(EQUAL(NUM 0,FIELD(VAR "n","n")),SEQ(ASSIGNF(VAR "n","is_even",NOT(FIELD(VAR "n","is_even"))),ASSIGNF(VAR "n","n",SUB(FIELD(VAR "n","n"),NUM 1)))),SEQ(CALLR("f",["num"]),FIELD(VAR "num","is_even")))));;
