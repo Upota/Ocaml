23048e2f4be5e3f1c50b28d77b740687fa9428aa
Upota <yujihoon3510@gmail.com>
2019-12-04 22:26:17 +0900

    type Inference complete

--- expand.ml
+++ expand.ml
@@ -15,6 +15,72 @@ and exp =
   | CALL of exp * exp
 and var = string
 
+module Env = struct
+  type t = (var * exp) list
+  let empty = []
+end
+
+let rec find : var -> Env.t -> exp
+  = fun x env -> match env with
+    | (y,e)::tl -> if x = y then e else find x tl
+    | [] -> VAR x
+
+let rec sub : exp -> Env.t -> exp
+= fun e env -> match e with
+  | CONST n -> CONST n
+  | VAR x -> find x env
+  | ADD (e1,e2) -> ADD (sub e1 env,sub e2 env)
+  | SUB (e1,e2) -> SUB (sub e1 env,sub e2 env)
+  | MUL (e1,e2) -> MUL (sub e1 env,sub e2 env)
+  | DIV (e1,e2) -> DIV (sub e1 env,sub e2 env)
+  | ISZERO e1 -> ISZERO (sub e1 env)
+  | READ -> READ
+  | IF (e1,e2,e3) -> IF (sub e1 env,sub e2 env,sub e3 env)
+  | LET (x,e1,e2) -> 
+    let se1 = sub e1 env in
+    let se2_ = sub e2 env in
+    let se2 = sub se2_ ((x,se1)::env) in
+    if se2 = se2_ then LET (x,se1,se2_) else se2
+  | LETREC (f,x,e1,e2) -> LETREC (f,x,sub e1 ((x,VAR x)::env),sub e2 ((f,VAR f)::env))
+  | PROC (x,e1) -> PROC (x,sub e1 ((x,VAR x)::env))
+  | CALL (e1,e2) -> CALL (sub e1 env,sub e2 env)
+
 let expand : exp -> exp 
-=fun e -> e (* TODO *)
+=fun e -> match e with
+  | LET (x,e1,e2) -> sub e []
+  | LETREC (f,x,e1,e2) -> sub e []
+  | _ -> e
 ;;
+
+expand (LET ("x", CONST 1, VAR "x"));;
+expand (
+LET ("f", PROC ("x", VAR "x"),
+IF (CALL (VAR "f", ISZERO (CONST 0)),
+CALL (VAR "f", CONST 11),
+CALL (VAR "f", CONST 22))));;
+expand (LET ("x", ADD (CONST 1, ISZERO (CONST 0)), CONST 2));;
+expand(PROC("x",VAR "x"));;
+expand(LET("x",CONST 1,VAR "x"));;
+expand(LET("x",PROC("x",VAR "x"),VAR "x"));;
+expand (LET("x",ADD(VAR "x",CONST 1),VAR "x"));;
+expand(LET("x",PROC("x",ADD(VAR "x",CONST 1)),VAR "x"));;
+expand(ADD(LET("x",CONST 1,CONST 1),VAR "x"));; 
+expand(LET("t",DIV(CONST 11,CONST 11),IF(ISZERO (VAR "t"),CONST 1,CONST 2)));;
+expand(LET("t",MUL(CONST 1,CONST 12),PROC ("t",VAR "t")));;
+expand(LET("x",CONST 1,ADD(PROC("x",VAR "x"),VAR "x")));;
+expand(LETREC("f","x",CONST 1,LET("x",CONST 2,CALL(VAR "f",VAR "x"))));;
+expand(LET("y",IF(CONST 11,CONST 12,CONST 13),PROC ("t",VAR "y")));;
+expand(LET("x",LET("y",VAR "x",VAR "x"),CALL(VAR "x",CONST 1)));;
+expand(LET("x",CONST 20,LETREC("f","x",CALL(VAR "f",VAR "x"),VAR "x")));;
+expand(LET("t",CONST 15,LETREC("t","x",CONST 10,VAR "t")));;
+
+
+(* expand(LET("f",PROC("f",VAR "f"),LET("f",CALL(PROC("f",VAR "f"),VAR "f"),VAR "f")));;
+let f = proc f f in
+  let f = CALL (proc f f) f in f
+     *)
+(* expand(LET("f",CALL(VAR "y",CONST 2),LET("f",VAR "f",CONST 1)));;
+expand(LET("f",ISZERO(ISZERO (CONST 1)),LET("f", CONST 2,VAR "f")));;
+expand(LETREC("f","x",CALL(VAR "f",SUB(VAR "x",CONST 1)),CALL (VAR "f",CONST 1)));;
+expand(LET("x",CONST 1,LET("x",CONST 2,LET("x",ADD(CONST 3,VAR "x"),ADD(CONST 1,CONST 2)))));;
+expand(LET("x",ADD(ISZERO (CONST 10),CONST 10),PROC("x",LET("x",CONST 10,VAR "x"))));; *)
--- type.ml
+++ type.ml
@@ -94,7 +94,7 @@ let rec gen_equations : TEnv.t -> exp -> typ -> typ_eqn
   | ISZERO e1 ->
     let t1 = gen_equations tenv e1 TyInt in
     [(ty,TyBool)] @ t1
-  | READ
+  | READ -> [(ty,ty)]
   | IF (e1,e2,e3) ->
     let t1 = gen_equations tenv e1 TyBool in
     let t2 = gen_equations tenv e2 ty in
@@ -103,22 +103,60 @@ let rec gen_equations : TEnv.t -> exp -> typ -> typ_eqn
   | LET (x,e1,e2) ->
     let nty = fresh_tyvar () in
     let t1 = gen_equations tenv e1 nty in
-    let t2 = gen_equations (TEnv.extend (x,nty) tenv) e1 ty in
+    let t2 = gen_equations (TEnv.extend (x,nty) tenv) e2 ty in
     t1 @ t2
-  | LETREC (f,x,e1,e2)
+  | LETREC (f,x,e1,e2) ->
+    let nty1 = fresh_tyvar () in
+    let nty2 = fresh_tyvar () in
+    let extenv = TEnv.extend (f,(TyFun (nty2,nty1))) tenv in
+    let t1 = gen_equations (TEnv.extend (x,nty2) extenv) e1 nty1 in
+    let t2 = gen_equations extenv e2 ty in
+    t1 @ t2
   | PROC (x,e1) ->
     let nty1 = fresh_tyvar () in
     let nty2 = fresh_tyvar () in
-    let t1 = gen_equations (TEnv.extend (x,nty1) e1 nty2) in
+    let t1 = gen_equations (TEnv.extend (x,nty1) tenv) e1 nty2 in
     [(ty,TyFun (nty1,nty2))] @ t1
-  | CALL (e1,e2)) ->
+  | CALL (e1,e2) ->
     let nty = fresh_tyvar () in
     let t1 = gen_equations tenv e1 (TyFun (nty,ty)) in
     let t2 = gen_equations tenv e2 nty in
     t1 @ t2
 
+let rec unify : (typ * typ) -> Subst.t -> Subst.t
+= fun ty subst -> match ty with
+  | (TyInt,TyInt) -> subst
+  | (TyBool,TyBool) -> subst
+  | (TyVar a,ty1) ->
+    begin match ty1 with
+    | TyVar b ->
+      if a = b then subst
+      else Subst.extend a ty1 subst
+    | TyFun (ty2,ty3) ->
+      if TyVar a = ty2 || TyVar a = ty3 then raise TypeError
+      else Subst.extend a (TyFun (ty2,ty3)) subst
+    | _ -> Subst.extend a ty1 subst
+    end
+  | (ty1,TyVar a) -> unify (TyVar a,ty1) subst
+  | (TyFun (ty1,ty2),TyFun (ty11,ty22)) ->
+    let s = unify (ty1,ty11) subst in
+    let st1 = Subst.apply ty2 s in
+    let st2 = Subst.apply ty22 s in
+    let ss = unify (st1,st2) s in
+    ss
+  | (_,_) -> raise TypeError
+
+let rec unifyall : typ_eqn -> Subst.t -> Subst.t
+= fun eqns subst -> match eqns with
+  | [] -> subst
+  | (ty1,ty2)::tl ->
+    let st1 = Subst.apply ty1 subst in
+    let st2 = Subst.apply ty2 subst in
+    let s = unify (st1,st2) subst in
+    unifyall tl s
+
 let solve : typ_eqn -> Subst.t
-=fun eqns -> Subst.empty (* TODO *)
+= fun eqns -> unifyall eqns Subst.empty
 
 let typeof : exp -> typ 
 =fun exp ->
@@ -136,3 +174,45 @@ let typeof : exp -> typ
       print_endline "";
       ty
   with TypeError -> (print_endline "The program does not have type. Rejected."); exit (1);;
+
+(* typeof(LET ("f", PROC ("x", VAR "x"),
+IF (CALL (VAR "f", ISZERO (CONST 0)),
+CALL (VAR "f", CONST 11),
+CALL (VAR "f", CONST 22))));; *)
+
+typeof(IF (CALL (PROC ("x", VAR "x"), ISZERO (CONST 0)),
+ CALL (PROC ("x", VAR "x"), CONST 11), CALL (PROC ("x", VAR "x"), CONST 22)));;
+(* typeof(PROC ("f",
+PROC ("x", SUB (CALL (VAR "f", CONST 3),
+CALL (VAR "f", VAR "x")))));;
+
+typeof(PROC ("f", CALL (VAR "f", CONST 11)));;
+
+typeof(DIV(ADD(CONST 1,CONST 3),CONST 0));;
+
+typeof(IF (ISZERO (CONST 10),CONST 1, READ));; *)
+(* typeof(LET("x",READ,IF(ISZERO(VAR "x"),ADD(VAR "x",CONST 1),IF(ISZERO(ADD(VAR "x",CONST 1)),VAR "x",CONST 0))));; *)
+(* typeof(PROC("x",PROC("y",ISZERO(VAR "x"))));;
+typeof(PROC("f",PROC("x",ADD(CONST 1,CALL (VAR "f",VAR "x")))));;
+typeof(PROC("f",PROC("x",IF(CALL(VAR "f",VAR "x"),CONST 1,CONST 0))));;
+typeof(LET("x",CONST 1,LETREC("f","x",IF(ISZERO (VAR "x"),CONST 1,CONST 2),VAR "x")));; *)
+(* typeof(LET ("x", CONST 1,
+IF (VAR "x", SUB (VAR "x", CONST 1), CONST 0)));; *)
+(* typeof(LET("x",READ,IF(ISZERO(VAR "x"),ADD(VAR "x",CONST 1),IF(ISZERO(ADD(VAR "x",CONST 1)),VAR "x",CONST 0))));; *)
+
+(* typeof(LET("f",PROC("y",VAR "y"),LET("g",CALL(VAR "f",CONST 1),LET("h",CALL(VAR "f",CONST 2),VAR "g"))));; *)
+(* TyInt *)
+(* let f = proc (y) (y) in
+  let g = f 1 in
+    let h = f 2 in
+      g *)
+
+(* typeof(LET("f",PROC("y",VAR "y"),LET("g",CALL(VAR "f",CONST 1),LET("h",CALL(VAR "f",ISZERO(CONST 0)),VAR "g"))));; *)
+(* exception *)
+
+(* typeof(LET("f",PROC("x",PROC("y",ADD(VAR "x",VAR "y"))),ADD(CALL (VAR "f",CONST 1),CONST 2)));; *)
+(* typeof(LET("x",CONST 1,LET("f",PROC("x",IF(VAR "x",CONST 1,CONST 2)),LET("g",PROC ("x",PROC("y",ADD(VAR "y",CONST 1))),CALL(VAR "g",CALL(VAR "f",ISZERO(CONST 0)))))));;
+typeof(LET("f",PROC("f",CALL(VAR "f",CONST 11)),LET("g",PROC("g",ADD(VAR "g",CONST 10)),PROC("c",ADD(CALL(VAR "f",VAR "g"),VAR "c")))));;
+typeof(LETREC("bin2dec","bin",PROC("depth",IF(ISZERO(VAR "bin"),CONST 0,LET("remainder",SUB(VAR "bin",MUL(DIV(VAR "bin",CONST 10),CONST 10)),ADD(MUL(VAR "remainder",VAR "depth"),CALL(CALL(VAR "bin2dec",DIV(VAR "bin",CONST 10)),MUL(VAR "depth",CONST 2)))))),VAR "bin2dec"));;
+typeof(LET("modular",PROC("x",PROC("y",SUB(VAR "x",MUL(VAR "y",DIV(VAR "x",VAR "y"))))),CALL(VAR "modular",READ)));;
+typeof(LETREC("reverse","n",PROC("result",IF(VAR "n",VAR "result",LET("remainder",SUB(VAR "n",MUL(DIV(VAR "n",CONST 10),CONST 10)),CALL(CALL(VAR "reverse",DIV(VAR "n",CONST 10)),ADD(MUL(VAR "result",CONST 10),VAR "remainder"))))),CALL(CALL(VAR "reverse",READ),CONST 0)));; *)
