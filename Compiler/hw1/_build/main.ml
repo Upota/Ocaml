4e08330b73af4101082506317fdbb4df49c6431e
Upota <yujihoon3510@gmail.com>
2020-04-10 19:06:18 +0900

    update testcases

--- Compiler/hw1/_build/main.ml
+++ Compiler/hw1/_build/main.ml
@@ -7,16 +7,23 @@ let testcases : (Regex.t * alphabet list) list =
     (Epsilon, []);
     (Alpha A, [A]);
     (Alpha A, [B]);
-    (OR (Alpha A, Alpha B), [B]);
-    (CONCAT (STAR (Alpha A), Alpha B), [B]);
-    (CONCAT (STAR (Alpha A), Alpha B), [A;B]);
+    (OR (Alpha A, Alpha B), [B]);  (*   (A + B)    *)
+    (CONCAT (STAR (Alpha A), Alpha B), [B]);   (*  A*B   *)
+    (CONCAT (STAR (Alpha A), Alpha B), [A;B]);  
     (CONCAT (STAR (Alpha A), Alpha B), [A;A;B]);
     (CONCAT (STAR (Alpha A), Alpha B), [A;B;B]);
-    (CONCAT (CONCAT (STAR (CONCAT (Alpha A, Alpha A)), STAR (CONCAT (Alpha B, Alpha B))), Alpha B), [B]);
+    (CONCAT (CONCAT (STAR (CONCAT (Alpha A, Alpha A)), STAR (CONCAT (Alpha B, Alpha B))), Alpha B), [B]);    (* (AA)*(BB)*B  *)
     (CONCAT (CONCAT (STAR (CONCAT (Alpha A, Alpha A)), STAR (CONCAT (Alpha B, Alpha B))), Alpha B), [A;A;B]);
     (CONCAT (CONCAT (STAR (CONCAT (Alpha A, Alpha A)), STAR (CONCAT (Alpha B, Alpha B))), Alpha B), [B;B;B]);
     (CONCAT (CONCAT (STAR (CONCAT (Alpha A, Alpha A)), STAR (CONCAT (Alpha B, Alpha B))), Alpha B), [A;A;A;A;B;B;B]);
-    (CONCAT (CONCAT (STAR (CONCAT (Alpha A, Alpha A)), STAR (CONCAT (Alpha B, Alpha B))), Alpha B), [A;A;A;B;B;B])
+    (CONCAT (CONCAT (STAR (CONCAT (Alpha A, Alpha A)), STAR (CONCAT (Alpha B, Alpha B))), Alpha B), [A;A;A;B;B;B]);
+
+    (CONCAT (STAR (OR (Alpha A, Alpha B)),Alpha A), [A;B;B;B;A;A]);           (*      (A + B)*A      TRUE *)
+    (CONCAT (STAR (OR (Alpha A, Alpha B)),Alpha A), [B;B;B;B;A;A;B]);          (* FALSE *)
+    (CONCAT (Alpha A,STAR (OR(Alpha A,Alpha B))), [A;B;A;B;A;A;A;A;A;A;B;A]);             (*       A(A + B)*    TRUE *)
+    (CONCAT (Alpha A,STAR (OR(Alpha A,Alpha B))), [B;B;A;A]);                  (* FALSE *)
+    (CONCAT (Alpha A, CONCAT (Alpha B, CONCAT (STAR (CONCAT (Alpha A, CONCAT (Alpha A, Alpha B))), CONCAT (Alpha B, Alpha A)))), [A;B;B;A]);  (* AB(AAB)*BA TRUE *)
+    (CONCAT (Alpha A, CONCAT (Alpha B, CONCAT (STAR (CONCAT (Alpha A, CONCAT (Alpha A, Alpha B))), CONCAT (Alpha B, Alpha A)))), [A;B;A;A;B;A;B;B;A]);  (* AB(AAB)*BA FALSE  *)
   ]
 
 let match_regex : Regex.t -> alphabet list -> bool
--- Compiler/hw1/hw1.ml
+++ Compiler/hw1/hw1.ml
@@ -52,25 +52,6 @@ and eval_reg2n : Regex.t -> (Nfa.state * Nfa.t) -> (Nfa.state * Nfa.t)
     let nfa6 = Nfa.add_epsilon_edge (Nfa.add_epsilon_edge nfa5 (s,s3)) (s2,s3) in
     (s3,nfa6)
 
-(* check nfa *)
-(* let nfa = regex2nfa (CONCAT (Alpha A,STAR (OR (Alpha A, Alpha B))))
-let _ = Nfa.print nfa *)
-
-(* let nfa1 = regex2nfa (CONCAT (STAR (OR (Alpha A, Alpha B)),Alpha A)) *)
-(* let nfa2 = regex2nfa (Alpha B) *)
-(* let nfa2 = regex2nfa (CONCAT(Alpha A,Alpha B)) *)
-(* let nfa2 = regex2nfa (STAR (CONCAT(Alpha A,Alpha B))) *)
-(* let nfa2 = regex2nfa (STAR (OR(Alpha A,Alpha B))) *)
-(* let nfa2 = regex2nfa (CONCAT (Alpha A,STAR (OR(Alpha A,Alpha B))))
-let nfa2 = regex2nfa (CONCAT (STAR (OR(Alpha A,Alpha B)),Alpha A))
-
-
-(* let nfa2 = regex2nfa (OR(Alpha A,Alpha B)) *)
-
-let _ = Nfa.print nfa2 *)
-
-
-
 let rec nfa2dfa : Nfa.t -> Dfa.t
 =fun nfa -> 
   let d0 = fix (e_closure nfa) (BatSet.singleton (Nfa.get_initial_state nfa)) in
@@ -83,16 +64,12 @@ and eval_n2d : Nfa.t -> Dfa.states -> Dfa.states -> Dfa.t -> Dfa.t
 =fun nfa d w dfa ->
   if BatSet.is_empty w then dfa
   else
-
   let (ds,w') = BatSet.pop w in
   let a_clo = fix (e_closure nfa) (closure nfa A ds) in
   let dfa1 = fs_check nfa (Dfa.add_edge (Dfa.add_state dfa a_clo) (ds, A, a_clo)) a_clo in
   
-
   let b_clo = fix (e_closure nfa) (closure nfa B ds) in
   let dfa2 = fs_check nfa (Dfa.add_edge (Dfa.add_state dfa1 b_clo) (ds, B, b_clo)) b_clo in
-  (* let dfa2 = if BatSet.is_empty b_clo then dfa1
-    else fs_check nfa (Dfa.add_edge (Dfa.add_state dfa1 b_clo) (ds, B, b_clo)) b_clo in *)
   
   let (up_d,up_w) = update_dw (update_dw (d,w') a_clo) b_clo in
   eval_n2d nfa up_d up_w dfa2
@@ -128,71 +105,6 @@ and fs_check : Nfa.t -> Dfa.t -> Dfa.state -> Dfa.t
   if BatSet.is_empty (BatSet.intersect s fs) then dfa
   else Dfa.add_final_state dfa s
 
-
-(* check dfa *)
-(* let dfa1 = nfa2dfa nfa
-let a_clo = fix (e_closure nfa) (closure nfa A (BatSet.singleton 9))
-let d = BatSet.singleton a_clo
-
-let dfa2 = Dfa.add_state (Dfa.add_state dfa1 (BatSet.singleton 9)) a_clo
-let dfa = Dfa.add_edge dfa2 ((BatSet.singleton 9), A, a_clo)
-let _ = Dfa.print dfa
-
-let (temp1,temp2) = update_dw (d,BatSet.empty) (BatSet.singleton 20)
-let (t1,t2) = BatSet.pop temp1
-let (t3,t4) = BatSet.pop t2
-let temp_dfa = Dfa.create_new_dfa t1
-let temp_dfa1 = Dfa.add_state temp_dfa t3
-let _ = Dfa.print temp_dfa1 *)
-(* 
-let dfa = nfa2dfa nfa2
-let _ = Dfa.print dfa *)
-
-(* debug *)
-(* let d0 = fix (e_closure nfa2) (BatSet.singleton (Nfa.get_initial_state nfa2))
-let dfa1 = Dfa.create_new_dfa d0
-let w = BatSet.singleton d0
-let (ds,w') = BatSet.pop w
-let a_set = closure nfa2 A ds
-(* let a_clo = fix (e_closure nfa2) a_set *)
-
-let ef = e_closure nfa2
-let tmp = Dfa.create_new_dfa a_set
-let fa1 = ef a_set
-let tmp1 = Dfa.add_state tmp fa1 
-let fa2 = ef fa1
-let tmp2 = Dfa.add_state tmp1 fa2 
-let _ = Dfa.print tmp1
-let _ = Dfa.print tmp2
-
-let fa3 = ef fa2
-let tmp3 = Dfa.add_state tmp2 fa3
-let _ = Dfa.print tmp3
-
-let fa4 = ef fa3
-let tmp4 = Dfa.add_state tmp3 fa4 
-let _ = Dfa.print tmp4
-
-let tp = if BatSet.equal fa3 fa4 then "true" else "false"
-let tp = if fa3 = fa4 then "true" else "false"
-
-let _ = print_endline tp
-let a_clo = fix ef a_set *)
-
-(* let _ = Dfa.print (Dfa.add_state dfa1 a_set) *)
-(* let _ = Dfa.print dfa1 *)
-
-let re = CONCAT (CONCAT (STAR (CONCAT (Alpha A, Alpha A)), STAR (CONCAT (Alpha B, Alpha B))), Alpha B)
-let input = [A;A;A;B;B;B]
-
-let nfa = regex2nfa re
-let dfa = nfa2dfa nfa
-let _ = Nfa.print nfa
-let _ = Dfa.print dfa
-
-
-
-
 (* Do not modify this function *)
 let regex2dfa : Regex.t -> Dfa.t
 =fun regex -> 
@@ -203,4 +115,3 @@ let regex2dfa : Regex.t -> Dfa.t
 let rec run_dfa : Dfa.t -> alphabet list -> bool
 =fun dfa str -> Dfa.is_final_state dfa (List.fold_left (fun s x -> 
 if BatSet.is_empty s then BatSet.empty else Dfa.get_next_state dfa s x) (Dfa.get_initial_state dfa) str)
-
