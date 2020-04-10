0530e56c8727b8bcc00d2fe1c1cb0965f7c49bb6
Upota <yujihoon3510@gmail.com>
2020-04-11 00:09:17 +0900

    add testcase

--- Compiler/hw1/_build/main.ml
+++ Compiler/hw1/_build/main.ml
@@ -18,12 +18,33 @@ let testcases : (Regex.t * alphabet list) list =
     (CONCAT (CONCAT (STAR (CONCAT (Alpha A, Alpha A)), STAR (CONCAT (Alpha B, Alpha B))), Alpha B), [A;A;A;A;B;B;B]);
     (CONCAT (CONCAT (STAR (CONCAT (Alpha A, Alpha A)), STAR (CONCAT (Alpha B, Alpha B))), Alpha B), [A;A;A;B;B;B]);
 
-    (CONCAT (STAR (OR (Alpha A, Alpha B)),Alpha A), [A;B;B;B;A;A]);           (*      (A + B)*A      TRUE *)
-    (CONCAT (STAR (OR (Alpha A, Alpha B)),Alpha A), [B;B;B;B;A;A;B]);          (* FALSE *)
-    (CONCAT (Alpha A,STAR (OR(Alpha A,Alpha B))), [A;B;A;B;A;A;A;A;A;A;B;A]);             (*       A(A + B)*    TRUE *)
-    (CONCAT (Alpha A,STAR (OR(Alpha A,Alpha B))), [B;B;A;A]);                  (* FALSE *)
-    (CONCAT (Alpha A, CONCAT (Alpha B, CONCAT (STAR (CONCAT (Alpha A, CONCAT (Alpha A, Alpha B))), CONCAT (Alpha B, Alpha A)))), [A;B;B;A]);  (* AB(AAB)*BA TRUE *)
-    (CONCAT (Alpha A, CONCAT (Alpha B, CONCAT (STAR (CONCAT (Alpha A, CONCAT (Alpha A, Alpha B))), CONCAT (Alpha B, Alpha A)))), [A;B;A;A;B;A;B;B;A]);  (* AB(AAB)*BA FALSE  *)
+    (CONCAT (Alpha A, CONCAT (Alpha B, STAR (Alpha A))), [A;B;A]) ;
+    (CONCAT (Alpha A, CONCAT (Alpha B, STAR (Alpha A))), [A;A]) ;
+
+    (CONCAT (Alpha B, CONCAT(OR (Alpha A, Alpha B), Alpha A)), [B;A;A]);
+    (CONCAT (Alpha B, CONCAT(OR (Alpha A, Alpha B), Alpha A)) ,[B;B;A;B]);
+
+    (CONCAT ( CONCAT (CONCAT (STAR (Alpha A),Alpha B),STAR (Alpha B)), OR (CONCAT (Alpha A, Alpha B),CONCAT (Alpha B, Alpha A))), [A;A;B;B;B;A;B]);
+    (CONCAT ( CONCAT (CONCAT (STAR (Alpha A),Alpha B),STAR (Alpha B)), OR (CONCAT (Alpha A, Alpha B),CONCAT (Alpha B, Alpha A))) ,[A;A;B;A]);
+
+    (CONCAT (STAR (OR (Alpha A, Alpha B)),Alpha A), [A;B;B;B;A;A]);           
+    (CONCAT (STAR (OR (Alpha A, Alpha B)),Alpha A), [B;B;B;B;A;A;B]);          
+    (CONCAT (Alpha A,STAR (OR(Alpha A,Alpha B))), [A;B;A;B;A;A;A;A;A;A;B;A]);            
+    (CONCAT (Alpha A,STAR (OR(Alpha A,Alpha B))), [B;B;A;A]);                 
+    (CONCAT (Alpha A, CONCAT (Alpha B, CONCAT (STAR (CONCAT (Alpha A, CONCAT (Alpha A, Alpha B))), CONCAT (Alpha B, Alpha A)))), [A;B;B;A]);  
+    (CONCAT (Alpha A, CONCAT (Alpha B, CONCAT (STAR (CONCAT (Alpha A, CONCAT (Alpha A, Alpha B))), CONCAT (Alpha B, Alpha A)))), [A;B;A;A;B;A;B;B;A]);
+
+    (CONCAT (STAR (OR (Alpha A, Alpha B)), Alpha A), [A;B;B;B;A;A]);
+    (CONCAT (STAR (OR (Alpha A, Alpha B)), Alpha A), [A;B;B;B;A;B]);
+    (STAR (CONCAT (Alpha A, STAR (Alpha B))), [A;B;B;B;A;A;B]);
+    (STAR (CONCAT (Alpha A, STAR (Alpha B))), [B;A;B;B;B;A;A;B]);
+    (STAR (CONCAT (OR (Alpha A, CONCAT (Alpha B, Alpha A)), Alpha B)), []);
+    (STAR (CONCAT (OR (Alpha A, CONCAT (Alpha B, Alpha A)), Alpha B)), [B;A;B;A;B]);
+    (STAR (CONCAT (OR (Alpha A, CONCAT (Alpha B, Alpha A)), Alpha B)), [A;B;B;A;A]);
+    (OR (CONCAT (OR (Alpha A, Epsilon), Alpha B), STAR(CONCAT (Alpha B, Alpha B))), [A;B]);
+    (OR (CONCAT (OR (Alpha A, Epsilon), Alpha B), STAR(CONCAT (Alpha B, Alpha B))), [A;A;B;A]);
+    (CONCAT (STAR (CONCAT (Alpha B, OR (STAR (Alpha A), Alpha B))), OR (CONCAT (Alpha B, STAR (Alpha A)), STAR (Alpha B))), [B;A;A;B;A;B;B;B;A]);
+    (CONCAT (STAR (CONCAT (Alpha B, OR (STAR (Alpha A), Alpha B))), OR (CONCAT (Alpha B, STAR (Alpha A)), STAR (Alpha B))), [A;B;B;A;B;A;A])
   ]
 
 let match_regex : Regex.t -> alphabet list -> bool
